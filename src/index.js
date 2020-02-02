d94d25daf96790c2887f9b37d135faf676c6a343
MZorskas <mzorskas@gmail.com>
2020-02-02 19:49:25 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import './assets/styles/app.scss';
+import './assets/images/account_circle-24px.svg'
+import './assets/images/background.gif'
+import './assets/images/background1.jpg'
+import './assets/images/portrait1.jpg'
