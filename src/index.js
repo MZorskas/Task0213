d5ab84f4e3c260bc4161827d97808747c16b7f1e
MZorskas <mzorskas@gmail.com>
2020-02-03 21:44:55 +0200

    about article

--- src/index.js
+++ src/index.js
@@ -3,4 +3,5 @@ import './assets/images/account_circle-24px.svg'
 import './assets/images/background.gif'
 import './assets/images/background1.jpg'
 import './assets/images/portrait1.jpg'
-import './index.hbs';
+import './index.hbs'
+import './assets/images/about.jpg';
